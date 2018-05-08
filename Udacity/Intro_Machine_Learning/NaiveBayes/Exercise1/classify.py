72395a1068054095e6048dcd8bec8d44eac273f5
Teng <yichuanandy@gmail.com>
2018-05-09 00:40:03 +0100

    add exercise1: speed prediction according terrain

---
+++ Udacity/Intro_Machine_Learning/NaiveBayes/Exercise1/classify.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Created by Teng on 09/05/2018
+
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+    """ compute the accuracy of your Naive Bayes classifier """
+    ### import the sklearn module for GaussianNB
+    from sklearn.naive_bayes import GaussianNB
+
+    ### create classifier
+    clf = GaussianNB()
+
+    ### fit the classifier on the training features and labels
+    clf.fit(features_train, labels_train)
+
+    ### use the trained classifier to predict labels for the test features
+    pred = clf.predict(features_test)
+
+    from sklearn.metrics import accuracy_score
+
+    accuracy = accuracy_score(labels_test, pred)
+
+    ### calculate and return the accuracy on the test data
+    ### this is slightly different than the example,
+    ### where we just print the accuracy
+    ### you might need to import an sklearn module
+    return accuracy
