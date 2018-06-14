dc9b3bead283fb912e206c07ea0ab50daf6c3e73
Teng <teng_huo@outlook.com>
2018-06-14 19:09:34 +0100

    completed courser practice

--- ud120-projects/evaluation/evaluate_poi_identifier.py
+++ ud120-projects/evaluation/evaluate_poi_identifier.py
@@ -24,8 +24,20 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
+### it's all yours from here forward!  
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+poi_list = filter(lambda i: i == 1, pred)
 
-### your code goes here 
+[i for i, j in zip(pred, labels_test) if i == j]
+print i
+print "TOTAL NUMBER:" + str(len(pred))
+print "POI NUMBER:" + str(len(poi_list))
+print clf.score(features_test, labels_test)
 
 
